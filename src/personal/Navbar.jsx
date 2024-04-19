6708fc977c635f3c60a30e126d2e55d5bddfffc3
walterosborne <willow3085@gmail.com>
2024-04-19 10:55:32 -0400

    Manifesting harder

--- src/personal/Navbar.jsx
+++ src/personal/Navbar.jsx
@@ -8,19 +8,19 @@ function Navbar() {
     return (
         <>
             <div id='bar'>
-                <Link class='link' to="/">
+                <Link className='link' to="/">
                     <div className='item' id='homebtn'>
                         <img id='home' className='picture' src={Home} alt="Home"></img>
                         <h2>Home</h2>
                     </div>
                 </Link>
-                <Link class='link' to="/aboutme">
+                <Link className='link' to="/aboutme">
                     <div className='item' id='about'>
                         <img id='head' className='picture' src={Head} alt="About Me"></img>
                         <h2>About Me</h2>
                     </div>
                 </Link>
-                <Link class='link' to="/professional">
+                <Link className='link' to="/professional">
                     <div className='item' id='prof'>
                         <img id='board' className='picture' src={Board} alt="Professional" width='35px' />
                         <h2>Profess-<br />ional</h2>
